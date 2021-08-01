dc511c6e61b66a26609f5c63350a619eaad0a0e3
kurs <kurs@DESKTOP-V3HDKPQ>
2021-08-01 11:56:58 +0200

    update

--- XMLParser/Program.cs
+++ XMLParser/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace XMLParser
 {
@@ -10,6 +12,23 @@ namespace XMLParser
     {
         static void Main(string[] args)
         {
+            String filename = @"c:\tmp\persons.xml";
+            String content = File.ReadAllText(filename);
+
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(content);
+
+            XmlElement persons = document["persons"];
+            Console.WriteLine($"Liczba elementów w persons = {persons.ChildNodes.Count} ");
+            foreach (XmlElement item in persons.ChildNodes)
+            {
+                string lname = item.GetAttribute("lname");
+                string name = item.GetAttribute("name");
+                string phone = item.GetAttribute("phone");
+                Console.WriteLine($"{name} {lname} - {phone}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
